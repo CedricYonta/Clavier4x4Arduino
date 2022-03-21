e1ddaa518a60f0cdb460edfb00ae44a11486aa7a
CedricYonta <93610090+CedricYonta@users.noreply.github.com>
2022-03-21 10:15:45 -0300

    Update main.cpp
    second commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+/*
+  Titre      : clacier4x4 Arduino
+  Auteur     : Cedric Yonta
+  Date       : 21/03/2022
+  Description: actionner une LED parmi avec un clavier 4x4 Arduino
+  Version    : 0.0.1
+  Sources : https://www.aranacorp.com/fr/utilisation-dun-clavier-numerique-4x4-avec-arduino/
+*/
+
+
+
+
 #include <Arduino.h>
+#include "WIFIConnector_MKR1000.h"
+#include "MQTTConnector.h"
+
+
 
 //Libraries
+
+
 #include <Keypad.h>//https://github.com/Chris--A/Keypad
 //Constants
 #define ROWS 4
@@ -10,10 +28,9 @@ const char kp4x4Keys[ROWS][COLS]  = {{'1', '2', '3', 'A'}, {'4', '5', '6', 'B'},
 byte rowKp4x4Pin [4] = {12, 9, 7, 6};
 byte colKp4x4Pin [4] = {5, 4, 3, 2};
 
-//int greenLed = 0 ;
-//int redLed = 1 ;
-int ledpin;
 
+
+// constante de verification des entrees
 const char GREENLED = 'A'; 
 const char REDLED = 'B'; 
 const char BLUELED = 'C';
@@ -23,27 +40,26 @@ const char CONFIRMINTENSITE = '#';
 const char CONFIRMLED = '*'; 
 
 
-int i =0;
- char t[4];
+// variables
 int compteur = 0;
+int  intensiteled;
+int ledpin = 17 ;
+
 void readKp4x4();
 
-//Variables
+
 Keypad kp4x4  = Keypad(makeKeymap(kp4x4Keys), rowKp4x4Pin, colKp4x4Pin, ROWS, COLS);
 void setup() {
- //Init Serial USB
  
-
+ 
+wifiConnect();                  //Branchement au réseau WIFI
+  MQTTConnect();
  Serial.begin(9600);
- //Serial.println(F("Initialize System"));
-
- /*pinMode(greenLed, OUTPUT);
- pinMode(redLed, OUTPUT);
- digitalWrite(greenLed,LOW);*/
+ 
 }
 void loop() {
  
- int  intensiteled ;
+  
 
 
  char intensite = kp4x4.getKey();
@@ -57,23 +73,23 @@ void loop() {
     
    switch (compteur)
     {
-       case 1: 
+       case 1: // selection de la LED
               if (intensite == REDLED)
               {
                  ledpin = 0;
-                 pinMode(ledpin,OUTPUT);
+                
               }else if (intensite == GREENLED)
               {
                  ledpin = 10;
-                 pinMode(ledpin,OUTPUT);
+                
               }else if (intensite == BLUELED)
               {
                  ledpin = 8 ;
-                 pinMode(ledpin,OUTPUT);
+               
               }else if (intensite == YELLOWLED)
               {
                  ledpin = 1;
-                 pinMode(ledpin,OUTPUT);
+                
               }else
               {
                 
@@ -81,168 +97,60 @@ void loop() {
               
 
         break;
-        case 2: 
+        case 2: // confirmation deu choix de la LED
                if (intensite == CONFIRMLED )
               {
                  Serial.println("led selectionné");
               } 
              
         break;
-      case 3:
-              intensiteled = map(intensite, 0, 9, 0, 255);
-               intensiteled = intensite;
-               analogWrite(ledpin,intensite);
+      case 3:   // entree de l'intensite de la LED
+
+               intensiteled = intensite - '0';
+                
+              intensiteled = map(intensiteled, 0, 9, 0, 255 );
+              
         
         break;
-      case 4:
+      case 4: // confirmation de l'intenite , actionnement de la LED et envois sur le serveur THingsboards
               if (intensite == CONFIRMINTENSITE )
                   {
                         Serial.println(" intensité selectionné");
-                        digitalWrite(ledpin, intensiteled);
+                        pinMode(ledpin,OUTPUT);
+                        analogWrite(ledpin,intensiteled);
+
+                                                      if (ledpin == 0)
+                                  {
+                                    appendPayload("redLED", intensiteled); 
+                                    sendPayload(); 
+                                  }
+                                  else if (ledpin == 10)
+                                  {
+                                  appendPayload("greenLED", intensiteled);
+                                  sendPayload(); 
+                                  } else if (ledpin == 8)
+                                  {
+                                  appendPayload("blueLED", intensiteled);  
+                                    sendPayload();
+                                  } else if (ledpin == 1)
+                                  {
+                                    appendPayload("yellowLED", intensiteled);  
+                                    sendPayload();
+                                  }
+                                  else
+                                  {
+                                    
+                                  }
+                       
                   } 
       break;
       default: 
-      delay(1000);
-        //Serial.println("redemarrer le programme pour allumer une led");
-    }
-    
+      delay(500);
+   
 
- /*if (intensite) {
-         Serial.println(intensite);
-      intensiteled = map(intensite, 0, 255, 0, 9);
-        aWrite(greenLed,intensite);
-    }*/
-    
-
+   
+   
       
-
-
-      /*f(key)  // Check for a valid key.
-  {
-    switch (key)
-    {
-      case '*':
-        digitalWrite(ledpin, LOW);
-        break;
-      case '#':
-        digitalWrite(ledpin, HIGH);
-        break;
-      default:
-        Serial.println(key);
-    }
-  }*/
- 
-  /*if (choixled) {
-    Serial.println(choixled); 
-    if (i < 4 )
-    {
-      t[i] = choixled;
-       Serial.println(t[i]); 
-    }
-    
-  }
- i++;*/
-
-  
-    /* Serial.print("valeur ");
-     Serial.print(n); 
-    Serial.print(" :"); 
-     Serial.println(customKey);*/
-  
-
-  
-
-   /*char choixled = kp4x4.getKey();
- if (choixled) {
-            if (choixled == INDIQGREENLED)
-            {
-              Serial.println(choixled);
-              char confirmled = kp4x4.getKey();
-                if (confirmled)
-                {
-                  if (confirmled == CONFIRMLED)
-                  {
-                    Serial.println(confirmled);
-                    char intensite = kp4x4.getKey();
-                    if (intensite)
-                    {
-                      Serial.println(intensite);
-                      char confirmintensite = kp4x4.getKey();
-                      if (confirmintensite)
-                        {
-                          if (confirmintensite == CONFIRMINTENSITE)
-                          {
-                              Serial.println(confirmintensite);
-                              intensite = map(intensite, 0, 255, 0, 9);
-                              digitalWrite(greenLed,intensite);
-                          }
-                          
-                          
-                        }
-                    }
-                    
-
-
-                  }
-                  
-                  
-                  
-                    
-                }
-                
-            }
-      
-    }*/
-    
-
 }
 
-   
-   
-
-/*void readKp4x4() { /* function readKp4x4 */
- //// Read button states from keypad
- /*char choixled = kp4x4.getKey();
- if (choixled) {
-            if (choixled == INDIQGREENLED)
-            {
-              Serial.println(choixled);
-              char confirmled = kp4x4.getKey();
-                if (confirmled)
-                {
-                  if (confirmled == CONFIRMLED)
-                  {
-                    Serial.println(confirmled);
-                    char intensite = kp4x4.getKey();
-                    if (intensite)
-                    {
-                      Serial.println(intensite);
-                      char confirmintensite = kp4x4.getKey();
-                      if (confirmintensite)
-                        {
-                          if (confirmintensite == CONFIRMINTENSITE)
-                          {
-                              Serial.println(confirmintensite);
-                              intensite = map(intensite, 0, 255, 0, 9);
-                              digitalWrite(greenLed,intensite);
-                          }
-                          
-                          
-                        }
-                    }
-                    
-
-
-                  }
-                  
-                  
-                  
-                    
-                }
-                
-            }
-      
-    }
-    
-
- }*/
+}
